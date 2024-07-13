1a855252f251e1414abcdac3992d3ebb78d4d810
Mujeeb Sulayman <sulaymanmujeeb6@Gmail.com>
2024-07-13 08:10:10 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import ButtonGradient from './assets/svg/ButtonGradient'
+
+const App = () => {
+	return (
+		<>
+			<div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+				<Header />
+			</div>
+      <ButtonGradient/>
+		</>
+	);
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { brainwave } from '../assets';
+import { navigation } from '../constants';
+import { useLocation } from 'react-router-dom';
+import Button from './Button';
+
+const Header = () => {
+	const pathname = useLocation();
+	return (
+		<div className='fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm'>
+			<div className='items-center flex px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
+				<a
+					className='block w-[12rem] xl:mr-8'
+					href='#hero'>
+					<img
+						src={brainwave}
+						alt='Brain3 logo'
+						width={190}
+						height={40}
+					/>
+				</a>
+
+				{/* DESKTOP NAVIGATION MENU  */}
+				<nav className='hidden fixed top-[5rem] left-0 right-0 button-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
+					<div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
+						{navigation.map((item) => (
+							<a
+								key={item.id}
+								href={item.url}
+								className={`block relative text-2xl
+                                 hover:text-color-1 text-n-1 font-code uppercase transition-colors ${
+																		item.onlyMobile ? 'lg:hidden' : ''
+																	} px-6 py-6 md:py-8 lg:mr-0.25 lg:text-[14px] lg:font-semibold ${
+									item.url === pathname.hash
+										? 'z-2 lg:text-n-1'
+										: 'lg:text-n-1/50'
+								} lg:leading-5 lg:hover:text-n-1 xl:px-9`}>
+								{item.title}
+							</a>
+						))}
+					</div>
+
+					{/* //----------------BUTTON-----------------// */}
+				</nav>
+				<a
+					href='#signup'
+					className='button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
+					New Account
+				</a>
+
+				<Button
+					className='hidden lg:flex'
+					href='#login'>
+					Sign In
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
